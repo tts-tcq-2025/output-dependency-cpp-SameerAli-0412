a6f919bc63e5abb6710564b3949214e880c01b1c
SameerAli-0412 <188832856+SameerAli-0412@users.noreply.github.com>
2025-08-11 10:47:02 +0530

    Create misaligned.c

---
+++ misaligned.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+const char* majorColor[MAJOR_COLORS] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[MINOR_COLORS] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+void generateColorMap(ColorMapEntry colorMap[]) {
+    int idx = 0;
+    for (int i = 0; i < MAJOR_COLORS; i++) {
+        for (int j = 0; j < MINOR_COLORS; j++) {
+            colorMap[idx].index = i * MINOR_COLORS + j;
+            colorMap[idx].major = majorColor[i];
+            colorMap[idx].minor = minorColor[i];
+            idx++;
+        }
+    }
+}
+
+void formatColorMapEntry(char* buffer, int index, const char* major, const char* minor) {
+    sprintf(buffer, "%d | %s | %s", index, major, minor);
+}
+
+void printOnConsole(const char* lineContent) {
+    printf("%s\n", lineContent);
+}
+
+int printColorMap(void (*outputFunc)(const char*)) {
+    ColorMapEntry colorMap[COLOR_MAP_SIZE];
+    char line[MAX_LINE_LEN];
+    generateColorMap(colorMap);
+    for (int i = 0; i < COLOR_MAP_SIZE; i++) {
+        formatColorMapEntry(line, colorMap[i].index, colorMap[i].major, colorMap[i].minor);
+        outputFunc(line);
+    }
+    return COLOR_MAP_SIZE;
+}
+
+int main() {
+    testPrintColorMap();
+    return 0;
+}
