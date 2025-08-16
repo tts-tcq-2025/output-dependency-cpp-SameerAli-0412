0be950c8925b0df8165aad6f3e74c48c07986a30
SameerAli-0412 <188832856+SameerAli-0412@users.noreply.github.com>
2025-08-16 17:11:57 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#ifndef COLOR_MAP_H
+#define COLOR_MAP_H
 
 #define MAJOR_COLORS 5
 #define MINOR_COLORS 5
-#define COLOR_MAP_SIZE (MAJOR_COLORS * MINOR_COLORS)
-#define MAX_LINE_LEN 50
 
 extern const char* majorColor[MAJOR_COLORS];
 extern const char* minorColor[MINOR_COLORS];
 
-typedef struct {
+struct ColorPair {
     int index;
     const char* major;
     const char* minor;
-} ColorMapEntry;
+};
 
-void generateColorMap(ColorMapEntry colorMap[]);
-void formatColorMapEntry(char* buffer, int index, const char* major, const char* minor);
-void printOnConsole(const char* lineContent);
+// Prints the color map using the provided output function.
+// Returns the number of color pairs printed.
 int printColorMap(void (*outputFunc)(const char*));
+
+#endif // COLOR_MAP_H
