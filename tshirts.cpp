464842c3340a0847708450288e15270704e380ef
SameerAli-0412 <188832856+SameerAli-0412@users.noreply.github.com>
2025-08-16 14:29:26 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-
+ 
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -12,11 +12,14 @@ char size(int cms) {
     }
     return sizeName;
 }
-
+ 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+    assert(size(38) == 'M');
+    assert(size(42) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
+
